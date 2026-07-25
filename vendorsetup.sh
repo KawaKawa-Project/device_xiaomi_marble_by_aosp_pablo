ea97e5332fa53b17a1256526ebf779e375303658
pabloescobar-reborn <yogivangala07@gmail.com>
2026-07-25 18:38:13 +0000

    vendorsetup: track keys

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,8 @@ bash device/xiaomi/miuicamera-marble/vendorsetup.sh
 
 export NINJA_ARGS="-w dupbuild=warn"
 export SKIP_ABI_CHECKS=true
+
+if [[ ! -d "vendor/infinity-priv/keys" ]]; then
+    GIT_TERMINAL_PROMPT=0 git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys/ 2>/dev/null \
+        || echo "Skipping private keys repo (no access)"
+fi
